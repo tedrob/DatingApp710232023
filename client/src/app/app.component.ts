0edc1082e04ac1542abfbad9c8edb077623c3746
tedrobinson <tfqbtd@aol.com>
2023-11-02 12:57:33 -0400

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
-import { User } from './_models/User';
+
 
 @Component({
   selector: 'app-root',
